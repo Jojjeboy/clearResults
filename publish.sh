9f2f2bec182de35807a2adc2398e2dbc73cca840
Johannes Kärnstam <johannes.karnstam@havochvatten.se>
2021-01-19 09:12:04 +0100

    publish script now includes versioning of package.json file

--- publish.sh
+++ publish.sh
@@ -21,11 +21,14 @@ echo "********** Running script **************"
 # fi
 
 
-
 if [ $# -eq 0 ]; then
   echo "Du måste skicka med commit meddelande.\n*** Script stoppat ***"
   exit 1
 fi
+
+
+
+
 
 
 if [[ $(git diff --stat) != '' ]]; then
@@ -39,48 +42,16 @@ if [[ $(git diff --stat) != '' ]]; then
     fi
   fi
 
-  echo "npm version $upLevel";
-  $(npm version $upLevel);
+    git add . &&
+    git commit -m"$1" &&
+    $(npm run $upLevel) &&
+    npm run postinstall &&
+    ng build --prod --output-path docs --base-href "$PUBLISH_URL" &&
+    git add . &&
+    git commit -m"$POSTINSTALL_AUTO_COMMIT_MESSAGE" &&
+    git push
 else
   VN="$VN-mod"
   echo "Inga förändringar upptäcktes.\n*** Script stoppat ***"
   exit 0
 fi
-
-
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]')
-
-echo $PACKAGE_VERSION
-
-
-
-
-# if [[ $(git diff --stat) != '' ]]; then
-
-#   upLevel="minor"
-#   if [ $# -eq 2 ]; then
-#     if [ "$2" = "patch" ]; then
-#       upLevel="patch"
-#     elif [ "$2" = "major" ]; then
-#       upLevel="major"
-#     fi
-#   fi
-
-#   npm run "$upLevel" &&
-#     git add . &&
-#     git commit -m"$1" &&
-#     npm run postinstall &&
-#     ng build --prod --output-path docs --base-href "$PUBLISH_URL" &&
-#     git add . &&
-#     git commit -m"$POSTINSTALL_AUTO_COMMIT_MESSAGE" &&
-#     git push
-# else
-#   VN="$VN-mod"
-#   echo "Inga förändringar upptäcktes.\n*** Script stoppat ***"
-#   exit 0
-# fi
