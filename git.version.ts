93472126c0999c0c8bddc6daf7e5c7f2cdf7f82f
Johannes Kärnstam <johannes.karnstam@havochvatten.se>
2021-01-19 09:53:56 +0100

    publish script now includes versioning of package.json file 51

--- git.version.ts
+++ git.version.ts
@@ -8,6 +8,7 @@ async function createVersionsFile(filename: string) {
   const revision = (await exec('git rev-parse --short HEAD')).stdout.toString().trim();
   const branch = (await exec('git rev-parse --abbrev-ref HEAD')).stdout.toString().trim();
   const date = (await exec('git log -1 --format=%cd')).stdout.toString().trim();
+  const message = (await exec('git log -1 --pretty=%B')).stdout.toString().trim();
 
   console.log(`Application Version: '${process.env.npm_package_version}', revision: '${revision}', branch: '${branch}'`);
 
@@ -16,6 +17,7 @@ async function createVersionsFile(filename: string) {
       export const applicationversion = {
         version: '${process.env.npm_package_version}',
         revision: '${revision}',
+        message: '${message}',
         branch: '${branch}',
         date: '${date}'
       };`;
