d3e29c800c0b146d55a963430b924863f31d4964
Johannes Kärnstam <johannes.karnstam@havochvatten.se>
2021-01-19 10:53:15 +0100

    Publish script with amend commit

--- git.version.ts
+++ git.version.ts
@@ -8,8 +8,7 @@ async function createVersionsFile(filename: string) {
   const revision = (await exec('git rev-parse --short HEAD')).stdout.toString().trim();
   const branch = (await exec('git rev-parse --abbrev-ref HEAD')).stdout.toString().trim();
   const date = (await exec('git log -1 --format=%cd')).stdout.toString().trim();
-  const message = (await exec('git log -1 --pretty=%B')).stdout.toString().trim();
-
+ 
   console.log(`Application Version: '${process.env.npm_package_version}', revision: '${revision}', branch: '${branch}'`);
 
   const content = dedent`
@@ -17,7 +16,6 @@ async function createVersionsFile(filename: string) {
       export const applicationversion = {
         version: '${process.env.npm_package_version}',
         revision: '${revision}',
-        message: '${message}',
         branch: '${branch}',
         date: '${date}'
       };`;
--- publish.sh
+++ publish.sh
@@ -48,7 +48,7 @@ if [[ $(git diff --stat) != '' ]]; then
     npm run postinstall &&
     ng build --prod --output-path docs --base-href "$PUBLISH_URL"
     git add . &&
-    git commit -m"$POSTINSTALL_AUTO_COMMIT_MESSAGE" &&
+    git commit --amend --no-edit &&
     git push
 else
   VN="$VN-mod"
